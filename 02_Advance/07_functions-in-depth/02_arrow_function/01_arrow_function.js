c8d7cfbb7486bafe949e1a996a954633690f5c84
abidkk <dev.abidkk@gmail.com>
2024-02-01 13:51:20 +0500

    arrow

---
+++ 02_Advance/07_functions-in-depth/02_arrow_function/01_arrow_function.js
@@ -0,0 +1,20 @@
+// 01 : Example
+let greet = (name) => {
+    return  `Hello ${name} how are you`
+}
+console.log(greet("Abid Ali"))
+
+
+
+
+// 02: Example
+
+let getPercentage = (obtainedMarks) => {
+    let totalMarks = 500
+    let precentage = `${obtainedMarks / totalMarks * 100}`
+    return `your have got ${precentage}%`
+
+}
+
+console.log(getPercentage(480))
+
