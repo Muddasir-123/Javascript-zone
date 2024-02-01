ae27db5af94d1e7bbc82500ec61f0e647f2d1f39
abidkk <dev.abidkk@gmail.com>
2024-02-01 10:53:09 +0500

    if else

---
+++ 01_Basics/05_conditionals/01_if-else/03_else-if.js
@@ -0,0 +1,14 @@
+let pin = "abc123"
+
+//  else if is working
+if (pin == "abc111") {
+    console.log("if is working")
+}else if(pin == "abc123"){
+    console.log("else if is working")
+}else{
+    console.log("else if is working")
+}
+
+
+//  note : make change and tru to run if then else seperately
+
