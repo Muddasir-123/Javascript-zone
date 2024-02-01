6f6299d972a18c851634783ccff4d53844173d5f
abidkk <dev.abidkk@gmail.com>
2024-02-01 13:19:27 +0500

    es5 function

---
+++ 02_Advance/07_functions-in-depth/01_traditional_function/01_es5-function.js
@@ -0,0 +1,13 @@
+// 01:Example
+function doSum(x, y){
+return x * y
+}
+console.log(doSum(10,20))  // 200
+
+
+
+// 02:Example
+function greet(name){
+    return `Hello ${name} welcome!`;
+}
+console.log("Abidkk")
