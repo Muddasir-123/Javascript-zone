d36c303885162ab25677a78abbed2e4e88c7e6fb
abidkk <dev.abidkk@gmail.com>
2024-02-01 14:03:03 +0500

    return vs console

--- 01_Basics/06_functions/01_function.js
+++ 01_Basics/06_functions/01_function.js
@@ -15,8 +15,6 @@ function fun2(){
 fun2()
 
 
-
-
 // ES6  Arrow functions
 let fun3 = ()  => {
     console.log("Hello, I am arrow function")
@@ -25,3 +23,31 @@ fun3()
 
 
 
+
+
+console.log(`--------------------------------------------------`)
+////  console  - vs - return   in a function
+
+
+//01:  function with console
+let doSum1 = (x,y)=>{
+    console.log(x + y)
+}
+let sum1 = doSum1(10,20) // storing into a variable
+console.log(`I am console function ${sum1 }`) // undefined
+
+
+
+
+//02:  function with return
+let doSum2 = (x, y)=>{
+    return x + y
+}
+let sum2 = doSum2(10, 20) // storing into a variable
+console.log(`I am from return function ${sum2}`) // 30
+
+
+
+// Note : You can console function returns "undefined" And return function returns the actual value
+
+    
