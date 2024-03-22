1ad8bf56c49bbc27784a9bb58d3c9c5e97a15ba8
abidkk <dev.abidkk@gmail.com>
2024-03-22 19:10:07 +0500

    iife

---
+++ 02_Advance/05_functions-in-depth/05_IIFE_imdeiately-invoke-function-expression/iife_fun.js
@@ -0,0 +1,10 @@
+// IIFE stands for Immediately Invoked Function Expression. 
+// It is a JavaScript design pattern where a function is defined and then immediately invoked. 
+// This pattern is often used to create a new scope, isolate variables, and avoid polluting the global namespace.
+
+
+(function() {
+    var message = "Hello, world!";
+    console.log(message);
+})();
+  
