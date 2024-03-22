38a8fc0eaae513fbe129d4ab355f58bc971fba8e
abidkk <dev.abidkk@gmail.com>
2024-03-22 13:40:16 +0500

    date methods

--- 02_Advance/04_date_methods/01_date.js
+++ 02_Advance/04_date_methods/01_date.js
@@ -1,19 +1,19 @@
 const date = new Date()
 
 console.log(date) // 2024-01-10T13:14:18.838Z
-console.log(date.getFullYear()) // 2024
-console.log(date.getMonth()) // 0
-console.log(date.getDate()) // 10
-console.log(date.getDay()) // 3
+console.log(date.getFullYear()) // return current year ( 2024 )
+console.log(date.getMonth()) // return current moth index ( start from 0) 
+console.log(date.getDate()) // return current date (22) 
+console.log(date.getDay()) // return current day index  (5) starts from 0 Sunday 
 
 
 //  hours
 console.log("hours")
 
-console.log(date.getHours()) // 18
-console.log(date.getMinutes()) // 17
-console.log(date.getSeconds()) // 22
-console.log(date.getMilliseconds()) // 611
+console.log(date.getHours())  // get current time in hours  (18)
+console.log(date.getMinutes()) // get current Minutes past  (35)
+console.log(date.getSeconds()) // get current seconds past  (18)
+console.log(date.getMilliseconds()) //get current milliseconds past ( 912 )
 
 
 
