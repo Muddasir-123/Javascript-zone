b487354ac05ea13cd86e4b4c909562838b0b7896
abidkk <dev.abidkk@gmail.com>
2024-03-22 19:09:11 +0500

    named function

---
+++ 02_Advance/05_functions-in-depth/03_named_function/name_fun.js
@@ -0,0 +1,8 @@
+// Named function declaration
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  // Calling the named function
+  greet("Alice"); // Output: Hello, Alice!
+  
