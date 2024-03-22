8e2f973b82745883f7af839e7d32cf0a7eb59d10
abidkk <dev.abidkk@gmail.com>
2024-03-22 19:09:40 +0500

    function expression

---
+++ 02_Advance/05_functions-in-depth/04_function_expression/fun_exp.js
@@ -0,0 +1,8 @@
+// In JavaScript, function expressions are another way to define functions.Unlike function declarations, 
+// function expressions do not require a function name and can be assigned to variables.
+// Here's an example of a function expression:
+
+const welcome = function(){
+    console.log("Hello!");
+}
+welcome()
