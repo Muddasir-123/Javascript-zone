166dd9278deb93e6dd97436525140e2ef2780448
Muddasir-123 <153119211+Muddasir-123@users.noreply.github.com>
2024-04-26 12:44:48 +0500

    Update 02_number.js

--- 01_Basics/02_data_types/01_primitive_datatypes/02_number.js
+++ 01_Basics/02_data_types/01_primitive_datatypes/02_number.js
@@ -4,7 +4,7 @@ const num1 = 1000
 const percentage = 95.6
 
 console.log(num1) 
-console.log(num2) // 95.6
+console.log(percentage) // 95.6
 
 
 // Note : We will learn more about numbers and its methods in upcoming steps
